bc93414a9c771bbefb969d15420a37a72c5d0b66
GiurgiuDiana <giurgiudiana97@yahoo.com>
2021-02-08 22:00:34 +0200

    JUnit Test

---
+++ app/src/main/java/com/example/androidadvancedproject/DataSource.java
@@ -0,0 +1,16 @@
+package com.example.androidadvancedproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataSource {
+    List<String> getLodge() {
+        List<String> cabana = new ArrayList<>();
+        cabana.add("Cabana Munte");
+        cabana.add("Cabana Viisoara");
+        cabana.add("Cabana Baisoara");
+        cabana.add("Cabana Belis");
+        return cabana;
+    }
+
+}
